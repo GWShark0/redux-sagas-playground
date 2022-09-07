c39b26fdacbb450106230340dbeca100c8faad14
Greg Douglas <GWShark0@gmail.com>
2022-09-07 08:35:24 -0400

    Redux sagas hello world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+  const count = useSelector((state) => state.value);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
+
+  const incrementAsync = () => {
+    dispatch({ type: 'INCREMENT_ASYNC' });
+  };
+
+  const decrement = () => {
+    dispatch({ type: 'DECREMENT' });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>{count}</div>
+      <div>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={increment}>Increment</button>
+        <button onClick={incrementAsync}>Increment after 1 second</button>
+      </div>
     </div>
   );
 }
